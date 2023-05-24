da714d2944d0980a99517cafcad9e7459ab32cf5
Pratik <Pratik@pratik>
2023-05-24 20:47:55 +0530

    ATM Commit

---
+++ src/Com/ATM_Project/AtmOperationInterf.java
@@ -0,0 +1,8 @@
+package Com.ATM_Project;
+
+public interface AtmOperationInterf {
+    public void viewBalance();
+    public void withdrawAmount(double withdrawAmount);
+    public void depositAmount(double depositAmount );
+    public  void viewMiniStatement();
+}
